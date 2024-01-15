1634ac534b867ab3388dc6ac4a93912ce82e9f0a
Gonzalo Catrina <gmartincatrina@solvd.com>
2024-01-15 12:27:28 -0300

    feat(cqrs): second implementation

---
+++ advanced-architecture/src/alarms/application/event-handlers/alarm-created.event-handler.ts
@@ -0,0 +1,15 @@
+import { Logger } from "@nestjs/common";
+import { EventsHandler, IEventHandler } from "@nestjs/cqrs";
+import { AlarmCreatedEvent } from "../../domain/events/alarm-created.event";
+
+//Events is plural because we could have multiple events here
+@EventsHandler(AlarmCreatedEvent)
+export class AlarmCreatedEventHandler
+  implements IEventHandler<AlarmCreatedEvent>
+{
+  private readonly logger = new Logger(AlarmCreatedEventHandler.name);
+
+  handle(event: AlarmCreatedEvent) {
+    this.logger.log(`Alarm created event: ${JSON.stringify(event)}`);
+  }
+}
